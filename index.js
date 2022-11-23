a741711a3add95cfd9337e37eb76c1b44394cae2
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-23 18:46:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+//Fix the scope
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+
+//best customer
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'bob';
+    leastFavoriteCustomer ='jane'
+}
